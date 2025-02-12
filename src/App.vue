9c4f6214a87281a5ece7cb872a113e2e6744193c
qq23471 <3249738448@qq.com>
2025-02-12 21:14:01 +0800

    feat(ui): 添加vant组件并配置引入

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>  
+    <router-view/>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
   </div>
 </template>
 <style >
