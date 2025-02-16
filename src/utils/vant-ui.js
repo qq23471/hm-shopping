04771975d035595da8ab4e0b6da3936274508673
qq23471 <3249738448@qq.com>
2025-02-16 20:30:11 +0800

    feat(view/layout/home.vue) 新增：首页渲染 feat(components/home.js)新增：axios请求首页数据

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 // 按需引入需要使用的 Vant 组件
-import { Toast, Tabbar, TabbarItem, NavBar } from 'vant'
+import { Toast, Tabbar, TabbarItem, NavBar, Search, Swipe, SwipeItem, Grid, GridItem } from 'vant'
 
 // 注册 Vant 组件到 Vue 实例
 Vue.use(Toast) // 消息提示组件
 Vue.use(NavBar) // 导航栏组件
 Vue.use(Tabbar) // 底部标签栏组件
 Vue.use(TabbarItem) // 底部标签栏子项组件
+Vue.use(Search) // 搜索组件
+Vue.use(Swipe) // 轮播图组件
+Vue.use(SwipeItem) // 轮播图子项组件
+Vue.use(Grid) // 宫格组件
+Vue.use(GridItem) // 宫格子项组件
